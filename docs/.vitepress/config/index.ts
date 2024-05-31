45296295379a5e6908c26e580873a42bbf37a3ca
croftonweed <53463228+croftonweed@users.noreply.github.com>
2024-05-31 12:30:04 +0800

    Update index.ts

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vitepress'
-import { shared } from './shared'
-import { en } from './en'
-import { zh } from './zh'
-
+import { sharedConfig } from './shared'
+import locales from './locales'
 
 export default defineConfig({
-  ...shared,
-  locales: {
-    root: { label: 'English', ...en },
-    zh: { label: '简体中文', ...zh },
-    ko: { label: '한국어', lang: 'ko-KR', link: 'https://vitepress.vuejs.kr/' }
-  }
+    ...sharedConfig,
+    locales
 })
